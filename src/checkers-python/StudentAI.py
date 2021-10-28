946abac640fbeb08745cbd48e0b15c727d4ba538
Codey Huntting <creativecodey@gmail.com>
2021-10-28 10:11:48 -0700

    Built a basic scaffold of a greedy best first algorithm but not yet tested

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -2,6 +2,9 @@ from random import randint
 from BoardClasses import Move
 from BoardClasses import Board
 
+# Static functions unrelated to the class itself
+
+# StudentAI class
 
 # The following part should be completed by students.
 # Students can modify anything except the class name and existing functions and variables.
@@ -22,7 +25,7 @@ class StudentAI:
     # The move passed in is the move that the opponent just made,
     # or an invalid move if we get to move first
     def get_move(self, move):
-        # If there are no elements in the move passed in, then
+        # If there are no elements in the move passed in, then do... something
         if len(move) != 0:
             self.board.make_move(move, self.opponent[self.color])
         else:
@@ -31,13 +34,53 @@ class StudentAI:
         # Get all possible moves
         moves = self.board.get_all_possible_moves(self.color)
 
-        # Choose a random checker move
-        index = randint(0, len(moves) - 1)
-        # Choose a random element in the list of items in the move
-        # (meaning you might randomly not do a double jump even if it is available)
-        inner_index = randint(0, len(moves[index]) - 1)
-        move = moves[index][inner_index]
+        # Check if the possible moves exist
+        if len(moves) <= 0:
+            raise RuntimeError("StudentAI: tried to get a move, but no possible moves could be found. "
+                               "Are you sure the game hasn't already ended?")
+
+        # A list of moves with the best heuristic
+        bestMoves = []
+        bestHeuristic = -1
+
+        # Go through all moves in the list of all possible moves
+        for m in moves:
+            # get the heuristic of the current move
+            currentHeuristic = self.move_heuristic(m)
+
+            # If the current move is better than the best so far,
+            # clear out the list and add this move to it
+            if currentHeuristic < bestHeuristic:
+                bestMoves.clear()
+                bestMoves.append(m)
+                bestHeuristic = currentHeuristic
+
+            # If the current move has the same heuristic as the best so far,
+            # add this move to the list of best moves
+            elif currentHeuristic == bestHeuristic:
+                bestMoves.append(m)
+
+        # If there was only one move then set it to that move
+        if len(bestMoves) == 1:
+            move = bestMoves[0]
+        # If multiple moves had the same heuristic use the tiebreaker to choose one from the list
+        else:
+            move = self.heuristic_tiebreaker(bestMoves)
+
+        # Set the move equal to just the furthest move in the list
+        move = move[len(move) - 1]
 
         # Make a new move using the randomly selected element of the randomly selected move
         self.board.make_move(move, self.color)
         return move
+
+    # Get the heuristic value of a move
+    # The SMALLER the heuristic value, the BETTER the move
+    def move_heuristic(self, move):
+        return 0
+
+    # Given a list of moves with the same heuristic,
+    # use some tie breaking method to choose one of the moves
+    def heuristic_tiebreaker(self, moves):
+        return moves[randint(0, len(moves) - 1)]
+
