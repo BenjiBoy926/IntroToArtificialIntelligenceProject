47639e6e40fa6bacac65bf45ef359d97dbdd21a6
Codey Huntting <creativecodey@gmail.com>
2021-11-06 16:15:04 -0700

    Started developing the algorithm to build the search tree

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -1,6 +1,7 @@
 from random import randint
 from BoardClasses import Move
 from BoardClasses import Board
+import functools
 import copy
 
 """
@@ -30,6 +31,36 @@ def board_heuristic(board):
     return board.white_count - board.black_count
 
 
+# Given two boards, return the board that would be best for the given player
+def better_board(board1, board2, player_num):
+    # Player 1 prefers smaller heuristics
+    if player_num == 1:
+        if board_heuristic(board1) < board_heuristic(board2):
+            return board1
+        else:
+            return board2
+    # Player 2 prefers bigger heuristics
+    elif player_num == 2:
+        if board_heuristic(board1) > board_heuristic(board2):
+            return board1
+        else:
+            return board2
+    else:
+        raise ValueError("Player number must be either a 1 or a 2")
+
+
+# Given two states for the same player, return the state that would be best for that player
+def better_state(state1, state2):
+    if state1.player_number == state2.player_number:
+        board = better_board(state1.board, state2.board, state1.player_number)
+        if board == state1.board:
+            return state1
+        else:
+            return state2
+    else:
+        raise ValueError("These states cannot be compared because they do not occur on the same player's turn")
+
+
 # Get the heuristic value of a move
 # The SMALLER the heuristic value, the BETTER the move
 def move_heuristic(move):
@@ -41,20 +72,23 @@ def move_heuristic(move):
         heuristic -= abs(move[index][1] - move[index + 1][1])
     return heuristic
 
+
 # Given a list of moves with the same heuristic,
 # use some tie breaking method to choose one of the moves
 def heuristic_tiebreaker(moves):
     return moves[randint(0, len(moves) - 1)]
 
-# Describes a state in the game. It has the current board and the current player's turn
+
+# Describes a state in the game. It has the current board, the current player's turn, and the move (if any)
+# that caused this state
 class GameState:
-    def __init__(self, board, player_number):
+    def __init__(self, board, player_number, inciting_move=None):
         self.board = board
         self.opponent = {1: 2, 2: 1}
         self.player_number = player_number
+        self.inciting_move = inciting_move
 
-    # Expand the node by filling the children with a list of all nodes with the boards
-    # for all moves that result from this board
+    # Get a list of all the states that can result from all the possible moves from this state
     def get_all_resulting_states(self):
         moves = self.board.get_all_possible_moves(self.player_number)
         resulting_states = []
@@ -67,7 +101,7 @@ class GameState:
                 new_board = resulting_board(self.board, move, self.player_number)
 
                 # Create a game state with the new board and next opponent's turn
-                child = GameState(new_board, self.opponent[self.player_number])
+                child = GameState(new_board, self.opponent[self.player_number], move)
                 resulting_states.append(child)
 
         # Return the list of resulting states
@@ -80,7 +114,7 @@ class GameStateNode:
         self.parent = parent
         self.children = []
 
-    # Fill this states list of children with all resulting states
+    # Fill this node's list of children with all resulting states
     def expand(self):
         # Get a list of all states that result from this state
         resulting_states = self.state.get_all_resulting_states()
@@ -89,6 +123,22 @@ class GameStateNode:
         # Add a node for each state to this list of children
         for state in resulting_states:
             self.children.append(GameStateNode(state, self))
+
+    # Use the minimax algorithm to decide which state is the best state to go to
+    # for the current player at this node
+    # Return the state object that is best for this player
+    def minimax_choice(self):
+        # If this node has no children, the states to reduce is the list of all states
+        # that directly result from this one
+        if len(self.children) <= 0:
+            states_to_reduce = self.state.get_all_resulting_states()
+        # If this node has children, the states to reduce is the list of all states
+        # chosen by child nodes of this node
+        else:
+            states_to_reduce = [child.minimax_choice() for child in self.children]
+
+        # Reduce the list down to the best state in the list for this player
+        return functools.reduce(better_state, states_to_reduce)
 
 
 # StudentAI class
@@ -107,6 +157,9 @@ class StudentAI:
 
         # Start by assuming we are player 2
         self.color = 2
+
+        # Depth is in PLIES, not PLAYS, meaning it's the number of my move - their move pairs
+        self.search_depth = 4
 
     # Get the next move that the AI wants to make
     # The move passed in is the move that the opponent just made,
@@ -161,3 +214,16 @@ class StudentAI:
         # This modifies our copy of the board so that it matches the game's copy
         self.board.make_move(move, self.color)
         return move
+
+    def build_search_tree(self, inciting_move):
+        # Make inciting move None instead of an invalid zero-length move
+        if len(inciting_move) <= 0:
+            inciting_move = None
+
+        # Create the root node from the current game state
+        root = GameStateNode(GameState(self.board, self.color, inciting_move))
+
+        for i in range(self.search_depth):
+            root.expand()
+
+        return root
