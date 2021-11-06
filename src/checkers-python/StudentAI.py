0427c92367c6ad21a84f187eebc3d9329fcb3a1f
Codey Huntting <creativecodey@gmail.com>
2021-11-06 15:11:35 -0700

    Developing game state and nodes for tree based search

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -9,45 +9,87 @@ From <project_dir>/Tools
 python3 AI_Runner.py 7 7 2 l Sample_AIs/Random_AI/main.py ../src/checkers-python/main.py
 """
 
-# Describes a node in a search tree.
-class GameStateNode:
-    def __init__(self, parent, board, color):
+
+# Static functions
+
+# Get the board that results from the given move on the given player's turn
+def resulting_board(board, move, player_number):
+    new_board = copy.deepcopy(board)
+    new_board.make_move(move, player_number)
+    return new_board
+
+
+# Return a heuristic value for the board in this node.
+# Smaller values are good for player 1 (black)
+# Larger values are good for player 2 (white)
+# NOTE: the comments in the board class we're using says that player 1 is white and player 2 is black,
+# but the code seems to suggest otherwise.
+# Comments: BoardClasses.py @ line 99
+# Code that implies otherwise: BoardClasses.py @ line 104-110
+def board_heuristic(board):
+    return board.white_count - board.black_count
+
+
+# Get the heuristic value of a move
+# The SMALLER the heuristic value, the BETTER the move
+def move_heuristic(move):
+    heuristic = 0
+    for index in range(len(move) - 1):
+        # Decrease heuristic by horizontal distance between this and next move
+        heuristic -= abs(move[index][0] - move[index + 1][0])
+        # Decrease heuristic by vertical distance between this and next move
+        heuristic -= abs(move[index][1] - move[index + 1][1])
+    return heuristic
+
+# Given a list of moves with the same heuristic,
+# use some tie breaking method to choose one of the moves
+def heuristic_tiebreaker(moves):
+    return moves[randint(0, len(moves) - 1)]
+
+# Describes a state in the game. It has the current board and the current player's turn
+class GameState:
+    def __init__(self, board, player_number):
         self.board = board
         self.opponent = {1: 2, 2: 1}
-        self.color = color
+        self.player_number = player_number
 
-        self.parent = parent
-        self.children = []
-
-    # Expand the node by filling the children with a list of all boards for all moves that result from this board
-    def expand(self):
-        moves = self.board.get_all_possible_moves(self.color)
+    # Expand the node by filling the children with a list of all nodes with the boards
+    # for all moves that result from this board
+    def get_all_resulting_states(self):
+        moves = self.board.get_all_possible_moves(self.player_number)
+        resulting_states = []
 
         # Iterate over all moves in the possible moves
         for checker_moves in moves:
             # Iterate over each move that this checker can make
             for move in checker_moves:
                 # Get a copy of the board that results from this move
-                resulting_board = self.resulting_board(move, self.color)
+                new_board = resulting_board(self.board, move, self.player_number)
 
-                # Create a tree node to hold the new board
-                child = GameStateNode(self, resulting_board, self.opponent[self.color])
-                self.children.append(child)
+                # Create a game state with the new board and next opponent's turn
+                child = GameState(new_board, self.opponent[self.player_number])
+                resulting_states.append(child)
 
-    def resulting_board(self, move, turn):
-        new_board = copy.deepcopy(self.board)
-        new_board.make_move(move, turn)
-        return new_board
+        # Return the list of resulting states
+        return resulting_states
 
-    # Return a heuristic value for the board in this node.
-    # Smaller values are good for player 1
-    # Larger values are good for player 2
-    def heuristic(self):
-        return self.board.white_count - self.board.black_count
 
-    # Determine if this board is in the win state for a given player
-    def is_win(self):
-        return self.board.is_win(self.color)
+class GameStateNode:
+    def __init__(self, state, parent=None):
+        self.state = state
+        self.parent = parent
+        self.children = []
+
+    # Fill this states list of children with all resulting states
+    def expand(self):
+        # Get a list of all states that result from this state
+        resulting_states = self.state.get_all_resulting_states()
+        self.children = []
+
+        # Add a node for each state to this list of children
+        for state in resulting_states:
+            self.children.append(GameStateNode(state, self))
+
 
 # StudentAI class
 
@@ -61,18 +103,19 @@ class StudentAI:
         self.p = p
         self.board = Board(col, row, p)
         self.board.initialize_game()
-        # Why does this start as an empty string then get set to a number later?
-        self.color = ''
         self.opponent = {1: 2, 2: 1}
+
+        # Start by assuming we are player 2
         self.color = 2
 
     # Get the next move that the AI wants to make
     # The move passed in is the move that the opponent just made,
     # or an invalid move if we get to move first
     def get_move(self, move):
-        # If there are no elements in the move passed in, then do... something
+        # If the opponent previously made a move, update our board to express it
         if len(move) != 0:
             self.board.make_move(move, self.opponent[self.color])
+        # If our opponent did not previously make a move, that means we are player 1!
         else:
             self.color = 1
 
@@ -93,7 +136,7 @@ class StudentAI:
         for checkers_moves in moves:
             for move in checkers_moves:
                 # get the heuristic of the current move
-                currentHeuristic = self.move_heuristic(move)
+                currentHeuristic = move_heuristic(move)
 
                 # If the current move is better than the best so far,
                 # clear out the list and add this move to it
@@ -112,26 +155,9 @@ class StudentAI:
             move = bestMoves[0]
         # If multiple moves had the same heuristic use the tiebreaker to choose one from the list
         else:
-            move = self.heuristic_tiebreaker(bestMoves)
+            move = heuristic_tiebreaker(bestMoves)
 
         # Make a new move using the randomly selected element of the randomly selected move
         # This modifies our copy of the board so that it matches the game's copy
         self.board.make_move(move, self.color)
         return move
-
-    # Get the heuristic value of a move
-    # The SMALLER the heuristic value, the BETTER the move
-    def move_heuristic(self, move):
-        heuristic = 0
-        for index in range(len(move) - 1):
-            # Decrease heuristic by horizontal distance between this and next move
-            heuristic -= abs(move[index][0] - move[index + 1][0])
-            # Decrease heuristic by vertical distance between this and next move
-            heuristic -= abs(move[index][1] - move[index + 1][1])
-        return heuristic
-
-    # Given a list of moves with the same heuristic,
-    # use some tie breaking method to choose one of the moves
-    def heuristic_tiebreaker(self, moves):
-        return moves[randint(0, len(moves) - 1)]
-
